80dbb8cdca6c401ffd761958535b5005fe8937a2
Alex Markin <a@alex-markin.com>
2026-08-27 20:11:27 +0400

    Increase top-layer grain resolution

--- prototype/components/GrainLayer.tsx
+++ prototype/components/GrainLayer.tsx
@@ -4,10 +4,10 @@ import { useEffect } from 'react';
 
 export function GrainLayer() {
   useEffect(() => {
-    const grainSvg = "<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512'>" +
-      "<filter id='n' color-interpolation-filters='sRGB'><feTurbulence type='fractalNoise' baseFrequency='0.65' " +
-      "numOctaves='4' stitchTiles='stitch'/></filter>" +
-      "<rect width='512' height='512' filter='url(#n)'/></svg>";
+    const grainSvg = "<svg xmlns='http://www.w3.org/2000/svg' width='1024' height='1024'>" +
+      "<filter id='n' color-interpolation-filters='sRGB'><feTurbulence type='fractalNoise' baseFrequency='0.42' " +
+      "numOctaves='5' stitchTiles='stitch'/></filter>" +
+      "<rect width='1024' height='1024' filter='url(#n)'/></svg>";
     document.documentElement.style.setProperty('--grain-url', `url("data:image/svg+xml,${encodeURIComponent(grainSvg)}")`);
   }, []);
 
